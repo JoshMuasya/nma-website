81a2650b81ff3177bae98072f89086183a8e00e1
Joshua Muasya <96262776+JoshMuasya@users.noreply.github.com>
2023-12-21 19:49:41 +0300

    Update Links.tsx

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import LinkType from '../interface'
+import LinkType from '/vercel/path0/app/interface/index'
 
 const Links = ({ href, title, linkClass }: LinkType) => {
   return (
